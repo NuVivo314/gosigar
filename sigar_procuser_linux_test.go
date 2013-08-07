8dcd4078f7749c6cbd5ea38cf8e07d0bdb6ddd5b
NuVivo314 <nu.vivo@gmail.com>
2013-08-07 15:24:36 +0200

    gofmt

--- sigar_procuser_linux_test.go
+++ sigar_procuser_linux_test.go
@@ -1,29 +1,29 @@
 package sigar
 
 import (
-  "errors"
-  "fmt"
-  "os"
-  "testing"
+	"errors"
+	"fmt"
+	"os"
+	"testing"
 )
 
 func TestProcUser(t *testing.T) {
-  userInfo := ProcUser{}
-  err := userInfo.Get(os.Getpid())
-  if err != nil {
-    t.Error(err)
-  }
+	userInfo := ProcUser{}
+	err := userInfo.Get(os.Getpid())
+	if err != nil {
+		t.Error(err)
+	}
 
-  if userInfo.UidReal != fmt.Sprint(os.Getuid()) {
-    t.Error(errors.New("Real UID != os.Getuid{}"))
-  }
+	if userInfo.UidReal != fmt.Sprint(os.Getuid()) {
+		t.Error(errors.New("Real UID != os.Getuid{}"))
+	}
 
-  if userInfo.GidReal != fmt.Sprint(os.Getgid()) {
-    t.Error(errors.New("Real GID != os.Getgid{}"))
-  }
+	if userInfo.GidReal != fmt.Sprint(os.Getgid()) {
+		t.Error(errors.New("Real GID != os.Getgid{}"))
+	}
 
-  err = userInfo.Get(invalidPid)
-  if err == nil {
-    t.Errorf("Invalid ProcUser.Get('%d')", invalidPid)
-  }
+	err = userInfo.Get(invalidPid)
+	if err == nil {
+		t.Errorf("Invalid ProcUser.Get('%d')", invalidPid)
+	}
 }
